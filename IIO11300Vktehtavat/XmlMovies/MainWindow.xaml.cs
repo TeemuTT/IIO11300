643515dd8e9a63f613bc9bf0eb79033636b93f0b
TeemuTT <tuomela.teemu@gmail.com>
2016-02-18 11:06:10 +0200

    XmlMovies tuntiesimerkki

---
+++ IIO11300Vktehtavat/XmlMovies/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿/*
+* Copyright (C) JAMK/IT/Teemu Tuomela
+* This file is part of the IIO11300 course project.
+* Created: 18.2.2016
+* Authors: Teemu Tuomela
+*/
+
+using System.Windows;
+
+namespace XmlMovies
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenMovies1_Click(object sender, RoutedEventArgs e)
+        {
+            Movies1 win = new Movies1();
+            win.ShowDialog();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnOpenMovies2_Click(object sender, RoutedEventArgs e)
+        {
+            Movies2 win = new Movies2();
+            win.ShowDialog();
+        }
+    }
+}
---
+++ IIO11300Vktehtavat/XmlMovies/Movies2.xaml.cs
@@ -0,0 +1,88 @@
+﻿/*
+* Copyright (C) JAMK/IT/Teemu Tuomela
+* This file is part of the IIO11300 course project.
+* Created: 18.2.2016
+* Authors: Teemu Tuomela
+*/
+
+using System;
+using System.Windows;
+using System.Xml;
+
+namespace XmlMovies
+{
+    /// <summary>
+    /// Interaction logic for Movies2.xaml
+    /// </summary>
+    public partial class Movies2 : Window
+    {
+        public Movies2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            xdpMovies.Document.Save(xdpMovies.Source.LocalPath);
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                XmlNode root = xdpMovies.Document.SelectSingleNode("/Movies");
+                if (MessageBox.Show("Poistetaanko elokuva : " + tbName.Text + "?", "Elokuvagalleria", MessageBoxButton.YesNo) == MessageBoxResult.Yes) {
+                    root.RemoveChild(lbMovies.SelectedItem as XmlNode);
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbMovies.SelectedIndex >= 0)
+            {
+                lbMovies.SelectedIndex = -1;
+            }
+            else
+            {
+                if (tbName.Text.Length == 0 |
+                    tbDirector.Text.Length == 0 |
+                    tbCountry.Text.Length == 0)
+                {
+                    MessageBox.Show("Täytä kaikki kentät!");
+                    return;
+                }
+                try
+                {
+                    XmlNode root = xdpMovies.Document.SelectSingleNode("/Movies");
+
+                    XmlNode newNode = xdpMovies.Document.CreateElement("Movie");
+                    XmlAttribute xa1 = xdpMovies.Document.CreateAttribute("Name");
+                    xa1.Value = tbName.Text;
+                    newNode.Attributes.Append(xa1);
+
+                    xa1 = xdpMovies.Document.CreateAttribute("Director");
+                    xa1.Value = tbDirector.Text;
+                    newNode.Attributes.Append(xa1);
+
+                    xa1 = xdpMovies.Document.CreateAttribute("Country");
+                    xa1.Value = tbCountry.Text;
+                    newNode.Attributes.Append(xa1);
+
+                    xa1 = xdpMovies.Document.CreateAttribute("Checked");
+                    xa1.Value = checkWatched.IsChecked.ToString();
+                    newNode.Attributes.Append(xa1);
+
+                    root.AppendChild(newNode);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+    }
+}
